25fbe1de3e0f9e455b40fcd3a0ef1dd9b73c5050
Ekaterina Schedlberger <ek.schedl@gmail.com>
2025-01-13 20:30:41 +0100

    +computed finish

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,16 @@
 <div> 
   <h1> Lesson 4: {{ title }} </h1>
   <ul>
-    <li>
+    <li :class="posts[0].checked ? 'checked' : ''">
         <img :src="require(`@/assets/images/${posts[0].icon}`)" :alt="posts[0].icon">     
         <span>{{posts[0].text}} {{" '- i am javascript code' "}}{{(1+2)}}</span>
     </li>
-    <li>
+    <li :class="posts[1].checked ? 'checked' : ''">
         <img :src="require(`@/assets/images/${posts[1].icon}`)":alt="posts[1].icon">   
         <span>{{posts[1].text}}</span>
     </li>
-    <li>
-        <img :src="require(`@/assets/images/${posts[2].icon}`)" :alt="posts[2].icon">   
+    <li class="list-item" :class="posts[2].checked ? 'checked' : ''">
+      <img :src="require(`@/assets/images/${posts[2].icon}`)" :alt="posts[2].icon">   
         <span>{{posts[2].text}}</span>
     </li>
   </ul>
@@ -28,17 +28,20 @@ export default {
         {
           id:0,
           text:'Text first ',
-          icon: 'setting.png'
+          icon: 'setting.png',
+          checked: true
         },
         {
           id:1,
           text:'Text second',
-          icon: 'user.png'
+          icon: 'user.png',
+          checked: true
         },
         {
           id:2,
           text:'Text third',
-          icon: 'phone.png'
+          icon: 'phone.png',
+          checked: true
         }
       ]
     }
